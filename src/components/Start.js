3898c23176c16da85f6b717e61cb123cf307cecb
ChriGa <supersagasso@yahoo.de>
2021-10-04 17:01:28 +0200

    jetzt stand wie php version - 04102021

--- src/components/Start.js
+++ src/components/Start.js
@@ -1,13 +1,13 @@
 import { Link } from 'react-router-dom';
 
-
+// HTML Entities verwenden: https://stackoverflow.com/questions/63240955/render-html-entities-inside-a-string-in-react
 const headerItems = {
-    1: ["/projekt-1", "Logistik", "CMS &#8226; Templating php/JS/CSS3", "portfolio-1.jpg"],
-    2: ["/projekt-5", "Music", "CMS &#8226; Design & Templating php/CSS3", "portfolio-2.jpg"],
-    3: ["/projekt-3", "Agentur", "experimentell &#8226; animiert &#8226; Storytelling", "portfolio-3.jpg"],
-    4: ["/projekt-4", "Architektur", "CMS &#8226; Templating php/JS/CSS3", "portfolio-4.jpg"],
-    5: ["/projekt-2", "Finanzen", "CMS &#8226; Templating php/JS/CSS3", "portfolio-5.jpg"],
-    6: ["/projekt-6", "Beauty", "CMS &#8226; Design & Templating JS/php/CSS3", "portfolio-6.jpg"]
+    1: ["/projekt-1", "Logistik", `CMS ${String.fromCharCode(8226)} Templating php/JS/CSS3`, "portfolio-1.jpg"],
+    2: ["/projekt-5", "Music", `CMS ${String.fromCharCode(8226)} Design & Templating php/CSS3`, "portfolio-2.jpg"],
+    3: ["/projekt-3", "Agentur", `experimentell ${String.fromCharCode(8226)} animiert ${String.fromCharCode(8226)} Storytelling`, "portfolio-3.jpg"],
+    4: ["/projekt-4", "Architektur", `CMS ${String.fromCharCode(8226)} Templating php/JS/CSS3`, "portfolio-4.jpg"],
+    5: ["/projekt-2", "Finanzen", `CMS ${String.fromCharCode(8226)} Templating php/JS/CSS3`, "portfolio-5.jpg"],
+    6: ["/projekt-6", "Beauty", `CMS ${String.fromCharCode(8226)} Design & Templating JS/php/CSS3`, "portfolio-6.jpg"]
 }
 
 const headerItemArray = Object.values(headerItems).map((el, i) => {
