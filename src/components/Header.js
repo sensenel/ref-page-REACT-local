e378003149143b5685c20a4c99b61de9254f1192
ChriGa <supersagasso@yahoo.de>
2021-10-13 17:13:18 +0200

    progress bar Values im react state gesetzt und von dort nun ausgelesen

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,110 +1,123 @@
+import React from 'react';
 import profil from '../assets/ch-gabler.png';
+//import Counter from './counter'
 
-const skills = {
-    'html(5)': '91%',
-    'CSS(3)' : '94%',
-    'JavaScript': '82%',
-    'PHP': '71%',
-    'mySQL': '55%'
-}
 
-const skillsBottom = {
-        "Frameworks": {'React': '67%', 'GSAP': '86%', 'jQuery': '79%', 'BootStrap': '66%', "Node.js": '28%' },
-        "CMS-Systeme": {'Joomla': '89%', 'WordPress': '33%', 'SilverStripe': '40%'},
-        "Versionierung": {'GitHub': '73%', 'BitBucket': '55%'}
-}
+class Header extends React.Component {
 
-let skillCounter = 1;
-
-const skillsArray = Object.keys(skills).map((el, i) => {
-    return (
-        <div key={i} className="skill-item top flex">
-            <h3>{el}</h3>
-            <div className="progress">     
-                <div className="progress-bar" role="progressbar">   
-                    <p id={`id="count-${skillCounter}"`}>{skills[el]}</p>
-                </div>
-            </div>
-            <div style={{display: 'none'}}>
-                {skillCounter++}
-            </div>
-        </div>
-    );
-});
-
-const skillsBottomArray = Object.keys(skillsBottom).map((el, i) => {
-
-    return (
-        <div key={i} className="prog-bottom--item">
-            <h3>{el}</h3>
-            { Object.keys(skillsBottom[el]).map((key, x) => {
-                return (
-                        <div className="skill-item bottom">                    
-                            <h4>{key}</h4>
-                                <div className="progress">     
-                                    <div className="progress-bar" role="progressbar">   
-                                        <p id={`id="count-${skillCounter}"`}>{skillsBottom[el][key]}</p>
-                                    </div>                    
-                                </div>
-                                <div style={{display: 'none'}}>
-                                    {skillCounter++}
-                                </div>
-                        </div>
-                    ) // END return 2
-                }) // END Object.keys -> keys
-            } { /* js in JSX OUT */ }
-        </div>
-        ) // END return 1
-});
-
-const Header = () => {
-    return (
-        <>
-            <div id="skill-loader">
-                <div className="angle top-left"></div>
-                <div className="angle top-right"></div>
-                <div className="angle bottom-left"></div>
-                <div className="angle bottom-right"></div>
-            </div>
-            <header>
-                <div className="header-container">
-                    <div className="profil">
-                        <img src={ profil } alt="Ch. Gabler, Webentwickler und ganz netter Typ so :)" />
-
-                        <div id="skill-set">
-                            <button id="skill-close">&#10006;</button>
-                            <div className="skill-content">
-                                <h2>Skillset</h2>
-                                {skillsArray}
-                                <div id="prog-bottom" className="flex">
-                                    {skillsBottomArray}
-                                </div>   
-                            </div>                        
-                        </div>
-
-                        <div className="profil-links">
-                            <div className="profil-item--container">
-                                <div className="profil-item"><p id="skills">Meine Skills</p></div>
-                            </div>
-                            <div className="profil-item--container">
-                                <div className="profil-item"><p><a href="mailto:info@chris-gabler.de?subject=Nachricht von Projekt-Seite">info@chris-gabler.de</a></p></div>
-                            </div>
-                            <div className="profil-item--container">
-                                <div className="profil-item"><p><a href="tel:+4915122917743">Tel: +49 151 22 91 77 43</a></p></div>
-                            </div>
+    constructor(props) {
+        super(props);
+        this.state = {
+                skills: [ // array-> object basierend
+                    {key: 'html(5)', value:  '91'},
+                    {key: 'CSS(3)', value:  '94'},
+                    {key: 'JavaScript', value: '82'},
+                    {key:'PHP', value: '71'},
+                    {key: 'mySQL', value: '55'}
+                ],
+                skillsBottom: { // object-> object basierend / wie in php Version
+                    "Frameworks": {'React': '67', 'GSAP': '86', 'jQuery': '79', 'BootStrap': '66', "Node.js": '28' },
+                    "CMS-Systeme": {'Joomla': '89', 'WordPress': '33', 'SilverStripe': '40'},
+                    "Versionierung": {'GitHub': '73', 'BitBucket': '55'}
+                },
+                skillCounter: 1
+        };
+        //this.skillsArray = this.skillsArray.bind(this);
+        //this.skillsBottomArray = this.skillsBottomArray.bind(this);
+    }
+    
+    skillsArray() { // array-> object basierend
+        return this.state.skills.map((el, i) => {
+            return (
+                <div key={i} className="skill-item top flex">
+                    <h3>{el.key}</h3>
+                    <div className="progress">     
+                        <div className={"progress-bar"} role="progressbar">   
+                            <p style={{fontSize: '21px'}} id={`id="count-${this.state.skillCounter++}"`}>
+                                {/* -- verworfen, siehe counter.js -- <Counter value={el.value} /> */}
+                                {el.value + "%"}
+                            </p>
                         </div>
                     </div>
-                    <div className="menu-container">
-                        <button id="mnu-button" className="">
-                            <span className="ham-menu-1"></span>
-                            <span className="ham-menu-2"></span>
-                            <span className="ham-menu-3"></span>
-                        </button>
-                    </div>
                 </div>
-            </header>
-        </>
-    )
+            );
+        });
+    }
+
+    skillsBottomArray() { // object-> object basierend / wie in php Version
+        return Object.keys(this.state.skillsBottom).map((el, i) => {
+
+            return (
+                <div key={i} className="prog-bottom--item">
+                    <h3>{el}</h3>
+                    { Object.keys(this.state.skillsBottom[el]).map((key, x) => {
+                        return (
+                                <div className="skill-item bottom">                    
+                                    <h4>{key}</h4>
+                                        <div className="progress">     
+                                            <div className="progress-bar" role="progressbar">   
+                                                <p style={{fontSize: '21px'}} id={`id="count-${this.state.skillCounter++}"`}>{this.state.skillsBottom[el][key] + "%"}</p>
+                                            </div>                    
+                                        </div>
+                                </div>
+                            ) // END return 2
+                        }) // END Object.keys -> keys
+                    } 
+                </div>
+                ) // END return 1
+        });
+    }  
+
+    render() {
+        
+        return (
+            <>
+                <div id="skill-loader">
+                    <div className="angle top-left"></div>
+                    <div className="angle top-right"></div>
+                    <div className="angle bottom-left"></div>
+                    <div className="angle bottom-right"></div>
+                </div>
+                <header>
+                    <div className="header-container">
+                        <div className="profil">
+                            <img src={ profil } alt="Ch. Gabler, Webentwickler und ganz netter Typ so :)" />
+    
+                            <div id="skill-set">
+                                <button id="skill-close">&#10006;</button>
+                                <div className="skill-content">
+                                    <h2>Skillset</h2>
+                                    {this.skillsArray()}
+                                    <div id="prog-bottom" className="flex">
+                                        {this.skillsBottomArray()}
+                                    </div>   
+                                </div>                        
+                            </div>
+    
+                            <div className="profil-links">
+                                <div className="profil-item--container">
+                                    <div className="profil-item"><p id="skills">Meine Skills</p></div>
+                                </div>
+                                <div className="profil-item--container">
+                                    <div className="profil-item"><p><a href="mailto:info@chris-gabler.de?subject=Nachricht von Projekt-Seite">info@chris-gabler.de</a></p></div>
+                                </div>
+                                <div className="profil-item--container">
+                                    <div className="profil-item"><p><a href="tel:+4915122917743">Tel: +49 151 22 91 77 43</a></p></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="menu-container">
+                            <button id="mnu-button" className="">
+                                <span className="ham-menu-1"></span>
+                                <span className="ham-menu-2"></span>
+                                <span className="ham-menu-3"></span>
+                            </button>
+                        </div>
+                    </div>
+                </header>                
+            </>
+        )
+    }
 }
 
 export default Header
---
+++ src/components/counter.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+
+// idee von hier: https://codepen.io/HunorMarton/pen/EqmyMN
+// für header.js (verworfen Class/ useEffect Konflikt) https://www.freecodecamp.org/news/pass-data-between-components-in-react/
+/**
+ * Verworfen: React ist nicht ohne weiteres in der Lage
+ * normale Variablen durchzureichen; Problem hier, um als Callback
+ * el.value durchzureichen, werden React Hooks benötigt
+ * welche aber in Class-Komponents nicht eingesetzt werden können
+ * header.js MUSS aber eine Class Komponent sein, weil sonst die Skill-
+ * Value Methoden nicht verwendet werden können.
+ * Bis auf weiteres wird der Wert statisch belassen weil ein überhand 
+ * nehmendes, rekursives AbstraktionsKonstrukt durch noch mehr Components
+ * kein Sinn macht. tbc 
+ */
+
+const Counter = () => {
+    const [count, setCount] = React.useState(0)
+    
+    // Use useRef for mutable variables that we want to persist
+    // without triggering a re-render on their change
+    const requestRef = React.useRef();
+    const previousTimeRef = React.useRef();
+    
+    const animate = time => {
+      if (previousTimeRef.current != undefined) {
+        const deltaTime = time - previousTimeRef.current;
+        
+        // Pass on a function to the setter of the state
+        // to make sure we always have the latest state
+        setCount(prevCount => (prevCount + deltaTime * 0.05) % 100);
+      }
+      previousTimeRef.current = time;
+      requestRef.current = requestAnimationFrame(animate);
+    }
+    
+    React.useEffect(() => {
+      requestRef.current = requestAnimationFrame(animate);
+      return () => cancelAnimationFrame(requestRef.current);
+    }, []); // Make sure the effect runs only once
+    
+    return <>{Math.round(count)}%</>
+  }
+
+export default Counter;
